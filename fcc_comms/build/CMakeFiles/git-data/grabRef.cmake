6ef98e134cb6b74d4a1649c7dabcd57013617a21
Sid Ray <rays@signalpwr.com>
2022-05-15 16:39:41 -0700

    Added sem/timer

--- fcc_comms/build/CMakeFiles/git-data/grabRef.cmake
+++ fcc_comms/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/14086/Documents/topper/projs/git/github/esp32/fcc_comms/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Espressif/frameworks/esp-idf-v4.4.1/.git")
+set(GIT_DIR "C:/Users/14086/Documents/topper/projs/git/github/esp32/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
--- fcc_comms/main/main.c
+++ fcc_comms/main/main.c
@@ -1,11 +1,43 @@
+// Make the 10ms task run off a timer that fires every 10ms
+// instead of a task-delay. All other tasks should run off
+// this task. Need a task-list to hold sync task handles
+// times.
+// idf.py set-target esp32c3
+// idf.py build flash monitor
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
 
 #define STACK_SIZE 1024
+
+// Later do this
+struct tasklist_t {
+    TaskHandle_t tHandle;
+    TickType_t   tTicks;
+};
+
+// Semaphores
+SemaphoreHandle_t sem;
+
+// Timers
+TimerHandle_t tTimerHandle;
+uint32_t timerId = 1;
+
+void timer_cb(TimerHandle_t timer) {
+    // This doesn't make sense because timer is the handle
+    // of the timer that called this callback.
+    if (pvTimerGetTimerID(timer) == (void *)&timerId) {
+        printf("timer_cb : corrent timer-id\n");
+    }
+    if (sem != NULL) {
+            xSemaphoreGive(sem);
+    }
+}
 
 void task_10ms(void *pvParam) {
     while (1) {
@@ -19,6 +51,23 @@ void task_20ms(void *pvParam) {
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
+void task_sync_30ms(void *pvParam) {
+    while (1) {
+        if (sem != NULL) {
+            printf("task_sync_30ms : sem available taking it\n");
+            // --------------------------
+            // If there sem was created 
+            // then use it else just use 
+            // plain old delay.
+            // --------------------------
+            xSemaphoreTake(sem, portMAX_DELAY);
+        } else {
+            printf("task_sync_30ms : no sem so delay\n");
+            vTaskDelay(1000 /portTICK_PERIOD_MS);
+        }
+        printf("task_sync_30ms\n");
+    }
+}
 
 void app_main() {
 
@@ -26,21 +75,48 @@ void app_main() {
     TaskHandle_t xHandle = NULL;
     BaseType_t xReturned1;
     TaskHandle_t xHandle1 = NULL;
+    BaseType_t xReturned2;
+    TaskHandle_t xHandle2 = NULL;
+
+    // Create the semaphores/timers/queues etc
+    sem = xSemaphoreCreateBinary();    
+    if (sem == NULL) {
+        printf("[ERROR] sem : not enough space to create semaphore\n");
+    }
+    tTimerHandle = xTimerCreate("timer", pdMS_TO_TICKS(3000), pdTRUE, (void *)&timerId, timer_cb);
+    if (tTimerHandle == NULL) {
+        printf("[ERROR] timer : not enough space to create timer\n");
+    }
 
     xReturned = xTaskCreate(
-                    task_10ms,       /* Function that implements the task. */
-                    "task_10ms",          /* Text name for the task. */
-                    STACK_SIZE,      /* Stack size in words, not bytes. */
-                    ( void * ) 1,    /* Parameter passed into the task. */
-                    tskIDLE_PRIORITY,/* Priority at which the task is created. */
-                    &xHandle );      /* Used to pass out the created task's handle. */
+                    task_10ms,          // Function that implements the task.
+                    "task_10ms",        // Text name for the task.
+                    STACK_SIZE,         // Stack size in words, not bytes.
+                    ( void * ) 1,       // Parameter passed into the task.
+                    tskIDLE_PRIORITY,   // Priority at which the task is created.
+                    &xHandle );         // Used to pass out the created task's handle.
+
     xReturned1 = xTaskCreate(
-                    task_20ms,       /* Function that implements the task. */
-                    "task_20ms",          /* Text name for the task. */
-                    STACK_SIZE,      /* Stack size in words, not bytes. */
-                    ( void * ) 1,    /* Parameter passed into the task. */
-                    tskIDLE_PRIORITY,/* Priority at which the task is created. */
-                    &xHandle1 );      /* Used to pass out the created task's handle. */
+                    task_20ms,          // Function that implements the task.
+                    "task_20ms",        // Text name for the task.
+                    STACK_SIZE,         // Stack size in words, not bytes.
+                    ( void * ) 1,       // Parameter passed into the task.
+                    tskIDLE_PRIORITY,   // Priority at which the task is created.
+                    &xHandle1 );        // Used to pass out the created task's handle.
+
+    xReturned2 = xTaskCreate(
+                    task_sync_30ms,     // Function that implements the task.
+                    "task_sync_30ms",   // Text name for the task.
+                    STACK_SIZE,         // Stack size in words, not bytes.
+                    ( void * ) 1,       // Parameter passed into the task.
+                    tskIDLE_PRIORITY,   // Priority at which the task is created.
+                    &xHandle2 );        // Used to pass out the created task's handle.
+
+    (void)xReturned;
+    (void)xReturned1;
+    (void)xReturned2;
+
+    xTimerStart(tTimerHandle, 0);
 
     while (1) {
         printf("[fcc_comms] hello world\n");
